164bc467d6a48c98775070e599b6e461dda410f6
Max Böck <hello@mxb.at>
2020-04-15 17:36:35 +0200

    add print button

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,3 +1,8 @@
 import 'focus-visible'
 
 document.documentElement.classList.remove('no-js')
+
+const printButton = document.querySelector('.js-print')
+printButton.addEventListener('click', () => {
+    window.print()
+})
