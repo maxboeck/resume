4d3412673795c304e35f01276186a292cd49d9d1
Max Böck <hello@mxb.at>
2020-04-16 18:08:18 +0200

    add sidebar actions

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,6 +2,21 @@ import 'focus-visible'
 
 document.documentElement.classList.remove('no-js')
 
+// Scroll State
+const onScroll = () => {
+    const scrollClassName = 'js-scrolled'
+    const scrollTreshold = 200
+    const isOverTreshold = window.scrollY > scrollTreshold
+
+    if (isOverTreshold) {
+        document.documentElement.classList.add(scrollClassName)
+    } else {
+        document.documentElement.classList.remove(scrollClassName)
+    }
+}
+window.addEventListener('scroll', onScroll, { passive: true })
+
+// Print Button
 const printButton = document.querySelector('.js-print')
 printButton.addEventListener('click', () => {
     window.print()
