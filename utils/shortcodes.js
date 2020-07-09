e64878a28da46b9fa4038696d3d3448becde9186
Max Böck <hello@mxb.at>
2020-07-09 20:30:01 +0200

    add colon to social label fallback

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -14,7 +14,7 @@ module.exports = {
             'whatsapp'
         ]
         if (isSocial && !availableSocialIcons.includes(id)) {
-            return `<span aria-hidden="true">${name}&nbsp;</span>`
+            return `<span aria-hidden="true">${name}:&nbsp;</span>`
         }
         return `<svg class="icon icon--${id}" role="img" aria-hidden="true" width="24" height="24">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-${id}"></use>
