4d8b10cd14dbe719d9c90f47bc0a8fb04b818223
Max Böck <hello@mxb.at>
2021-07-08 18:36:55 +0200

    add github repositories

---
+++ src/data/repos.js
@@ -0,0 +1,27 @@
+const Cache = require('@11ty/eleventy-cache-assets')
+const orderBy = require('lodash/orderBy')
+
+// if you want to display your most starred github repositories,
+// change this to your username. if not, set it to false.
+const YOUR_GITHUB_USERNAME = 'maxboeck'
+
+module.exports = async function () {
+    if (!YOUR_GITHUB_USERNAME) {
+        return null
+    }
+
+    try {
+        console.log('Fetching GitHub repos...')
+        const repos = await Cache(
+            `https://api.github.com/users/${YOUR_GITHUB_USERNAME}/repos`,
+            {
+                duration: '1s',
+                type: 'json'
+            }
+        )
+        return orderBy(repos, 'stargazers_count', 'desc')
+    } catch (e) {
+        console.log('Failed fetching GitHub repos')
+        return []
+    }
+}
